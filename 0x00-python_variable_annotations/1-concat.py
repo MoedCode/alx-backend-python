c914030c0d158bb10aa8b573eb0c6d4de53bfd86
MoedCode <mohamed71291@gmail.com>
2024-04-19 02:47:46 +0200

    1 - 2

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,1 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""concat module file"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """function concat
+    Args:
+        str1 (str): first string
+        str2 (str): second string
+    Returns:
+        str: concatenation of str1 and str2
+    """
+    return (str1 + str2)
--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,1 +1,16 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of the given float number.
+
+    Parameters:
+        n (float): The input number.
+
+    Returns:
+        int: The floor of the input number.
+    """
+    return math.floor(n)
