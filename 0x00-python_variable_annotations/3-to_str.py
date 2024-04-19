8fc7194cdd04de0113b3dcb3d8b68d358c005ad2
MoedCode <mohamed71291@gmail.com>
2024-04-19 03:05:06 +0200

    3   %% 4

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of a float.
+
+    Args:
+        n (float): The float to convert to string.
+
+    Returns:
+        str: The string representation of the input float.
+    """
+    return str(n)
--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,1 +1,6 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+# 4-define_variables.py
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
