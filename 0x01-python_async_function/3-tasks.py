b1c55bc4ae14fe59931f8f2c6faa64d15ac33f57
MoedCode <mohamed71291@gmail.com>
2024-04-23 03:15:42 +0200

    Tue Apr 23 03:15:42 EET 2024

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+#!/usr/bin/env python3
+"""async_generator module file for 0x02-python_async_comprehension project"""
+from typing import Generator
+import asyncio
+import random
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """
+    async_generator function
+    Returns:
+        Generator[float, None, None]: [description]
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
