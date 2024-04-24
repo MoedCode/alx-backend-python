f410fe1a46c59e24131997a7a190ee88332a35af
MoedCode <mohamed71291@gmail.com>
2024-04-24 03:40:56 +0200

    1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,18 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+async_comprehension module file for 0x02-python_async_comprehension project
+"""
+import asyncio
+from typing import Generator, List
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    async_comprehension function
+    Returns:
+        List[float]: [description]
+    """
+    return [i async for i in async_generator()]
