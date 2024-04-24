a166b31ee981f558d38db4605a04bc2e89c53990
MoedCode <mohamed71291@gmail.com>
2024-04-24 03:31:25 +0200

    0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,1 +1,19 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""measure_runtime module file for 0x02-python_async_comprehension project"""
+import asyncio
+import time
+
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    measure_runtime function
+    Returns:
+        float: [description]
+    """
+    start_time = time.time()
+    await asyncio.gather(*(async_comprehension() for i in range(4)))
+    end_time = time.time()
+    return end_time - start_time
