1bd2b2eacaf1330b98766fbd1761f61e1b088e08
MoedCode <mohamed71291@gmail.com>
2024-05-13 01:18:47 +0300

    0

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+class TestAccessNestedMap(unittest.TestCase):
+    @parameterized.expand([
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected_value):
+        self.assertEqual(access_nested_map(nested_map, path), expected_value)
+
+if __name__ == "__main__":
+    unittest.main()
+
+'''
+print(access_nested_map({"a": {"b": 2}}, ("a",)))
+print(access_nested_map(nested_map={"a": {"b": 2}}, path=("a", "b")))
+print(access_nested_map(nested_map={"a": {"b":None}}, path=("a", "b", "c")))
+'''
