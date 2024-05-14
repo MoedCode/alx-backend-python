e363a2fa95220365a5e6609795085e92b1718eb0
MoedCode <mohamed71291@gmail.com>
2024-05-14 04:44:36 +0300

    3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,7 +6,8 @@ from parameterized import parameterized
 from typing import Dict, Tuple, Union
 from utils import (
     access_nested_map,
-    get_json
+    get_json,
+    memoize
 )
 
 
@@ -53,6 +54,26 @@ class TestGetJson(unittest.TestCase):
             get_req.assert_called_once_with(url_val)
 
 
+class TestMemoize(unittest.TestCase):
+    """Tests the `memoize` function."""
+    def test_memoize(self) -> None:
+        """Tests `memoize`'s output."""
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(
+                TestClass,
+                "a_method",
+                return_value=lambda: 42,
+                ) as memo_fxn:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property(), 42)
+            self.assertEqual(test_class.a_property(), 42)
+            memo_fxn.assert_called_once()
 if __name__ == "__main__":
     unittest.main()
 
